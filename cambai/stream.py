370feabd070723cb97be5821b1ad069dcfda09d0
Kyrylo V. Polezhaiev <kyrylo.polezhaiev@camb.ai>
2026-01-08 13:59:10 +0100

    stream: Add async version of functions

--- cambai/stream.py
+++ cambai/stream.py
@@ -1,6 +1,7 @@
 import typing
 import shutil
 import subprocess
+import asyncio
 
 
 def stream(audio_stream: typing.Iterator[bytes]) -> bytes:
@@ -66,3 +67,75 @@ def capture(sample_rate=16000) -> typing.Iterator[bytes]:
     finally:
         process.stdout.close()
         process.wait()
+
+
+async def stream_async(audio_stream: typing.AsyncIterator[bytes]) -> bytes:
+    if shutil.which("gst-play-1.0") is None:
+        message = (
+            "Audio streaming requires `gst-play-1.0`, but it was not found on your system. "
+            "On macOS, type `brew install gstreamer` to install it. "
+            "On Ubuntu, type `sudo apt install gstreamer1.0-plugins-base-apps` to install it."
+        )
+        raise ValueError(message)
+
+    gst_play_command = ["gst-play-1.0", "--no-interactive", "fd://0"]
+    
+    process = await asyncio.create_subprocess_exec(
+        *gst_play_command,
+        stdin=asyncio.subprocess.PIPE,
+        stdout=asyncio.subprocess.DEVNULL,
+        stderr=asyncio.subprocess.DEVNULL,
+    )
+
+    audio = b""
+    try:
+        async for chunk in audio_stream:
+            if chunk is not None:
+                process.stdin.write(chunk)
+                await process.stdin.drain()
+                audio += chunk
+    finally:
+        if process.stdin:
+            process.stdin.close()
+        await process.wait()
+    
+    return audio
+
+
+async def capture_async(sample_rate: int = 16000) -> typing.AsyncIterator[bytes]:
+    if shutil.which("gst-launch-1.0") is None:
+        message = (
+            "Audio capture requires `gst-launch-1.0`, but it was not found on your system. "
+            "On macOS, type `brew install gstreamer` to install it. "
+            "On Ubuntu, type `sudo apt install gstreamer1.0-tools` to install it."
+        )
+        raise ValueError(message)
+
+    gst_launch_command = [
+        "gst-launch-1.0",
+        "autoaudiosrc", "!",
+        "audioconvert", "!", "audioresample", "!",
+        f"audio/x-raw,format=S16LE,rate={sample_rate}", "!",
+        "fdsink", "fd=1"
+    ]
+    
+    process = await asyncio.create_subprocess_exec(
+        *gst_launch_command,
+        stdin=asyncio.subprocess.DEVNULL,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.DEVNULL,
+    )
+    
+    try:
+        while True:
+            chunk = await process.stdout.readexactly(16384)
+            if not chunk:
+                break  # EOS
+            yield chunk
+    except asyncio.IncompleteReadError as e:
+        # Handle EOS
+        if e.partial:
+            yield e.partial
+    finally:
+        process.stdout.close()
+        await process.wait()
